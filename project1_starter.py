bd60375f2a721d854b84436a2f06d4f5781e6714
Justin Sohn <sohnjustin2@gmail.com>
2023-09-27 18:47:04 -0700

    added main to read input.txt (not fully done)

--- project1_starter.py
+++ project1_starter.py
@@ -1,27 +1,52 @@
 from datetime import datetime
 
-###
-# with open("input.txt", 'r') as file:
-# content = file.read()
-# words = content.split()
-# file.close()
-# print(content)
 
-
-def schedule():
-    person1_busy_Schedule = #1st busy time goes here
-    person1_work_hours = #1st work time goes here
-    person2_busy_Schedule = #2nd busy time goes here
-    person2_work_hours = #2nd work time goes here
-    duration = #duration goes here
-
-    #convert inputs to datetime objects
+def schedule(person1_busy_Schedule, person1_work_hours, person2_busy_Schedule, person2_work_hours, duration):
+    # convert inputs to datetime objects
     format = "%H:%M"
     workInput1 = [datetime.strptime(x, format) for x in person1_work_hours]
-    busyInput1 = [[datetime.strptime(x, format) for x in y ] for y in person1_busy_Schedule]
+    busyInput1 = [[datetime.strptime(x, format)
+                   for x in y] for y in person1_busy_Schedule]
     workInput2 = [datetime.strptime(x, format) for x in person2_work_hours]
-    busyInput2 = [[datetime.strptime(x, format) for x in y ] for y in person2_busy_Schedule]
+    busyInput2 = [[datetime.strptime(x, format)
+                   for x in y] for y in person2_busy_Schedule]
 
-    #find min time for work hours
+    # find min time for work hours
     start_time = max(workInput1[0], workInput2[0])
     end_time = min(workInput1[1], workInput2[1])
+
+
+def main():
+    # Read input from file
+    with open("input.txt", 'r') as file:
+        content = file.readlines()
+        file.close()
+
+    compareTime = True
+    while compareTime:
+        if len(content) == 0:
+            compareTime = False
+            break
+        elif len(content) == 1:
+            compareTime = False
+            file.remove(content[0])
+            break
+        else:
+            person1_busy_Schedule = content[0]
+            person1_work_hours = content[1]
+            person2_busy_Schedule = content[2]
+            person2_work_hours = content[3]
+            duration = content[4]
+            # prepare the next input
+            content.remove(content[5])
+            schedule(person1_busy_Schedule, person1_work_hours,
+                     person2_busy_Schedule, person2_work_hours, duration)
+            content.remove(content[0])
+            content.remove(content[1])
+            content.remove(content[2])
+            content.remove(content[3])
+            content.remove(content[4])
+
+
+if __name__ == "__main__":
+    main()

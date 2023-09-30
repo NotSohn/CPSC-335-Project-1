355e535ba3d87d499077dcd489e1d49723d8da1c
HowVo <146135155+HowVo@users.noreply.github.com>
2023-09-30 04:52:10 -0700

    Update project1_starter.py

--- project1_starter.py
+++ project1_starter.py
@@ -15,6 +15,28 @@ def schedule(person1_busy_Schedule, person1_work_hours, person2_busy_Schedule, p
     start_time = max(workInput1[0], workInput2[0])
     end_time = min(workInput1[1], workInput2[1])
 
+    # create an array of unavailable time from the two inputs and sort it
+    unavailable_time = busyInput1 + busyInput2
+    unavailable_time.sort()
+
+    available_time = []
+
+    # find the time intervals when all members are available
+    if (unavailable_time[0][0] - start_time >= timedelta(minutes=duration)):
+        available_time.append ((start_time, unavailable_time[0][0]))
+
+    for i in range (1, len(unavailable_time)):
+        time_difference = unavailable_time[i][0] - unavailable_time[i-1][1] 
+        if (time_difference >= timedelta(minutes=duration)):
+            available_time.append ((unavailable_time[i-1][1], unavailable_time[i][0]))
+        
+    if (end_time - unavailable_time[len(unavailable_time) - 1][1] >= timedelta(minutes=duration)):
+        available_time.append((unavailable_time[len(unavailable_time) - 1][1], end_time))
+
+    # convert datetime objects back into time strings to display 
+    output = [[i.strftime(format) for i in l] for l in available_time]
+    print(output)
+
 
 def main():
     # Read input from file
